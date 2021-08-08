a4e6692f1f239de7df85cd2ec7b555801b367281
christopherbanas <cjb5537@rit.edu>
2021-08-08 18:12:29 -0400

    Table image adjust

--- lib/standings_page/data_table.dart
+++ lib/standings_page/data_table.dart
@@ -1,11 +1,41 @@
-import 'dart:collection';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_nba/database_models/team.dart';
 import 'package:flutter_nba/enums.dart';
 import 'package:lazy_data_table/lazy_data_table.dart';
-import 'package:table_sticky_headers/table_sticky_headers.dart';
+
+Map<String, String> scriptMap = {
+  '1': '\u00B9',
+  '2': '\u00B2',
+  '3': '\u00B3',
+  '4': '\u2074',
+  '5': '\u2075',
+  '6': '\u2076',
+  '7': '\u2077',
+  '8': '\u2078',
+  '9': '\u2079',
+  '10': '\u00B9\u2070',
+  '11': '\u00B9\u00B9',
+  '12': '\u00B9\u00B2',
+  '13': '\u00B9\u00B3',
+  '14': '\u00B9\u2074',
+  '15': '\u00B9\u2075',
+  '16': '\u00B9\u2076',
+  '17': '\u00B9\u2077',
+  '18': '\u00B9\u2078',
+  '19': '\u00B9\u2079',
+  '20': '\u00B2\u2070',
+  '21': '\u00B2\u00B9',
+  '22': '\u00B2\u00B2',
+  '23': '\u00B2\u00B3',
+  '24': '\u00B2\u2074',
+  '25': '\u00B2\u2075',
+  '26': '\u00B2\u2076',
+  '27': '\u00B2\u2077',
+  '28': '\u00B2\u2078',
+  '29': '\u00B2\u2079',
+  '30': '\u00B3\u2070',
+};
 
 class StandingsTable extends StatefulWidget {
   final List<Team> teams;
@@ -21,69 +51,65 @@ class StandingsTable extends StatefulWidget {
 }
 
 class _StandingsTableState extends State<StandingsTable> {
-  // late double _scrollOffsetX;
-  // late double _scrollOffsetY;
-
-  @override
-  void initState(){
-    super.initState();
-    // _scrollOffsetX = 0.0;
-    // _scrollOffsetY = 0.0;
-  }
 
   @override
   Widget build(BuildContext context) {
-    return LazyDataTable(
-      rows: widget.teams.length,
-      columns: widget.headers.length,
-      tableDimensions: LazyDataTableDimensions(
-        cellHeight: 50,
-        cellWidth: 100,
-        topHeaderHeight: 50,
-        leftHeaderWidth: 115,
-      ),
-      topHeaderBuilder: (i) => Align(alignment: Alignment.centerRight, child: Text(widget.headers[i].toString().split('.').last)),
-      leftHeaderBuilder: (i) => Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              child: Image.network(
-                  'https://cdn.nba.com/logos/nba/${widget.teams[i].valueMap[teamEnums.teamId]}/primary/L/logo.svg'
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+      child: LazyDataTable(
+        rows: widget.teams.length,
+        columns: widget.headers.length,
+        tableDimensions: LazyDataTableDimensions(
+          cellHeight: 50,
+          cellWidth: 100,
+          topHeaderHeight: 50,
+          leftHeaderWidth: 130,
+        ),
+        topHeaderBuilder: (i) => Align(
+            alignment: Alignment.centerRight,
+            child: Text(widget.headers[i].toString().split('.').last)
+        ),
+        leftHeaderBuilder: (i) => Row(
+            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(3, 0, 0, 0),
+                  child: Image.network(
+                      'https://cdn.nba.com/logos/nba/${widget.teams[i].valueMap[teamEnums.teamId]}/primary/L/logo.svg',
+                      height: 50.0,
+                      width: 50.0,
+                      fit: BoxFit.contain,
+                  ),
+                ),
               ),
+              Spacer(),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 6, 0),
+                child: Text('${widget.teams[i].valueMap[teamEnums.name]}${scriptMap['${i+1}']}'),
+              ),
+            ],
+        ),
+        dataCellBuilder: (row, col) => widget.headers.length <= 15 ?
+            Align(
+                alignment: Alignment.centerRight,
+                child: Text('${widget.teams[row].valueMap[teamEnums.normalStats].valueMap.values.toList()[col]}')
+            )
+            : Align(
+            alignment: Alignment.centerRight,
+            child: Text('${widget.teams[row].valueMap[teamEnums.advancedStats].valueMap.values.toList()[col]}')
             ),
-            Text(widget.teams[i].valueMap[teamEnums.name]),
-          ],
-
-      ),
-      dataCellBuilder: (row, col) => widget.headers.length <= 15 ?
-          Align(alignment: Alignment.centerRight, child: Text('${widget.teams[row].valueMap[teamEnums.normalStats].valueMap.values.toList()[col]}'))
-          : Align(alignment: Alignment.centerRight, child: Text('${widget.teams[row].valueMap[teamEnums.advancedStats].valueMap.values.toList()[col]}')),
-      topLeftCornerWidget: Center(child: Text("")),
-      tableTheme: LazyDataTableTheme(
-        cellColor: Colors.transparent,
-        alternateCellColor: Colors.transparent,
-        columnHeaderColor: Colors.transparent,
-        cornerColor: Colors.transparent,
-        rowHeaderColor: Colors.transparent,
-        alternateRowHeaderColor: Colors.transparent,
+        topLeftCornerWidget: Center(child: Text("")),
+        tableTheme: LazyDataTableTheme(
+          cellColor: Colors.transparent,
+          alternateCellColor: Colors.transparent,
+          columnHeaderColor: Colors.transparent,
+          cornerColor: Colors.transparent,
+          rowHeaderColor: Colors.transparent,
+          alternateRowHeaderColor: Colors.transparent,
+        ),
       ),
     );
-    // return StickyHeadersTable(
-    //   columnsLength: widget.headers.length,
-    //   rowsLength: widget.teams.length,  // how many teams
-    //   columnsTitleBuilder: (i) => Text(widget.headers[i].toString().split('.').last),
-    //   rowsTitleBuilder: (i) => Text(widget.teams[i].valueMap[teamEnums.name]),
-    //   contentCellBuilder: (col, row) => widget.headers.length <= 15 ?
-    //     Text('${widget.teams[row].valueMap[teamEnums.normalStats].valueMap.values.toList()[col]}')
-    //     : Text('${widget.teams[row].valueMap[teamEnums.advancedStats].valueMap.values.toList()[col]}'),
-    //   legendCell: Text("Sticky"),
-    //   initialScrollOffsetX: _scrollOffsetX,
-    //   initialScrollOffsetY: _scrollOffsetY,
-    //   onEndScrolling: (scrollOffsetX, scrollOffsetY) {
-    //     _scrollOffsetX = scrollOffsetX;
-    //     _scrollOffsetY = scrollOffsetY;
-    //   },
-    // );
   }
 }
 
